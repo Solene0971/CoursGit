3d7f32c32fb13941205391bbe8afc8de0f1eded7
solene <solene.bourg.pro@gmail.com>
2026-06-17 15:55:45 +0200

    tp3 debug exception name

--- src/test/java/com/example/demo/web/WebTests.java
+++ src/test/java/com/example/demo/web/WebTests.java
@@ -56,7 +56,7 @@ class WebTests {
 
     @Test
     void testgetStatistiquesVide() throws Exception {
-        when(statistiqueImpl.prixMoyen()).thenThrow(new ArithmeticException()); // quand la méthode prixMoyen est appelée lève une exception
+        when(statistiqueImpl.prixMoyen()).thenThrow(new PasDeVoitureException()); // quand la méthode prixMoyen est appelée lève une exception
         mockMvc.perform(get("/statistique"))
                 .andDo(print())
                 .andExpect(status().isInternalServerError());                   // attend un code de retour 500 (erreur serveur)
